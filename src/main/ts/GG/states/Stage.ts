443b29aaf7bb823fa5da9e81aa474ab814b40a01
Leonardo Montenegro <leonardo.montenegro@chaotive.cl>
2017-07-04 00:27:50 -0400

    Every update makes the tint of the current column to change color :D

--- src/main/ts/GG/states/Stage.ts
+++ src/main/ts/GG/states/Stage.ts
@@ -9,15 +9,18 @@ export class Stage extends Phaser.State {
     grid: Grid;
     colors: string[];
 
+    private currentColumn: number;
+
     preload() {
         Preload.images(this.game);
     }
 
     create() {
-        this.cells = [];
         this.grid = new Grid(new Size(640, 360), Utils.getRandomInt(2, 320), Utils.getRandomInt(2, 180));
         this.colors = ['Purple', 'Red', 'Yellow', 'Blue', 'LightBlue', 'White'];
+        this.currentColumn = 0;
 
+        this.cells = [];
         for(let i = 0; i < this.grid.cellsByIndex.length; i++) {
             let cell = new CellSprite(this.game, this.grid.cellsByIndex[i], this.colors);
             this.cells.push(cell);
@@ -25,7 +28,19 @@ export class Stage extends Phaser.State {
         }
     }
 
+    update() {
+        if(this.currentColumn == this.grid.columns) this.currentColumn = 0;
 
+        let columnCells = this.cells.filter(c =>
+            c.cell.grid.x == this.currentColumn
+        );
+
+        columnCells.map(c =>
+            c.tint = Math.random() * 0xffffff
+        );
+
+        this.currentColumn++;
+    }
 
 }
 
